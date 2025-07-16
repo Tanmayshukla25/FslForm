46b76db18226d8b4f3b39caa84570383520b56e1
Tanmayshukla25 <tanmayshukla252@gmail.com>
2025-07-16 14:25:51 +0530

    UseRender

--- Fsl-Form/src/axiosConfig.js
+++ Fsl-Form/src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance =axios.create({
-    baseURL:"http://localhost:4000/"
+    baseURL:import.meta.env.VITE_BACKEND_URL,
+    withCredentials:true
 })
 
-export default instance;
+export default instance;    
--- backend/index.js
+++ backend/index.js
@@ -5,18 +5,20 @@ import cors from "cors";
 import connectToDb from "./config/db.js";
 import detailRouter from "./routes/detailsRoutes.js";
 
-
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
-await connectToDb()
+await connectToDb();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-
-
 app.use("/api/details", detailRouter);
-
 
 app.listen(port, () => console.log(`Server started at port ${port}`));
